6611e561df3ae1865fc2fcdff98c3a867a1c845f
Andrew Downing <ad510n1@gmail.com>
2015-07-18 16:27:46 -0700

    separate type inference from code gen

--- pdl.h
+++ pdl.h
@@ -1,9 +1,5 @@
 #include <stdint.h>
 #include <stdio.h>
-typedef int8_t i1;
-typedef int16_t i2;
-typedef int32_t i4;
-typedef int64_t i8;
 #define PDL_NUM_OP(t,n,o)t n##_##t(t a,t b){return a o b;}
 #define PDL_NUM_OPS(t) \
   PDL_NUM_OP(t,add,+) \
@@ -16,8 +12,8 @@ typedef int64_t i8;
   PDL_NUM_OP(t,gt,>) \
   PDL_NUM_OP(t,lte,<=) \
   PDL_NUM_OP(t,gte,>=)
-PDL_NUM_OPS(i1)
-PDL_NUM_OPS(i2)
-PDL_NUM_OPS(i4)
-PDL_NUM_OPS(i8)
-i4 pn(i4 i){printf("%d\n", i);return i;}
+PDL_NUM_OPS(int8_t)
+PDL_NUM_OPS(int16_t)
+PDL_NUM_OPS(int32_t)
+PDL_NUM_OPS(int64_t)
+int32_t pn(int32_t i){printf("%d\n", i);return i;}
